677cd3e3c5436dbcdb3ee1418abd8eb00cb574ec
AvaritiaFuze <154471921+AvaritiaFuze@users.noreply.github.com>
2024-08-02 23:48:04 +0300

    Update game.js

--- game.js
+++ game.js
@@ -36,8 +36,8 @@ let wolf = {
 let objects = [];
 let beams = [
     {img: beamTopLeftImg, x: 130, y: 65, width: 300, height: 10, angle: 45}, // top left
-    {img: beamTopRightImg, x: canvas.width - 150, y: 45, width: 300, height: 10, angle: 45}, // top right
-    {img: beamBottomLeftImg, x: 130, y: canvas.height - 40, width: 300, height: 10, angle: -45}, // bottom left
+    {img: beamTopRightImg, x: canvas.width - 150, y: 45, width: 300, height: 10, angle: -45}, // top right
+    {img: beamBottomLeftImg, x: 130, y: canvas.height - 40, width: 300, height: 10, angle: 45}, // bottom left
     {img: beamBottomRightImg, x: canvas.width - 150, y: canvas.height - 60, width: 300, height: 10, angle: -45} // bottom right
 ];
 let score = 0;
